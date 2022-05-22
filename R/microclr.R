23280d37d603ba76b28ca1a8492899ec2e5c63b1
Lars Snipen <lars.snipen@nmbu.no>
2022-05-22 09:29:13 +0200

    Updated clr documentation

--- R/microclr.R
+++ R/microclr.R
@@ -9,17 +9,24 @@
 #' @param n.pseudo number of pseudo-readcounts to add.
 #' 
 #' @details This is a standard implementation of the Aitchisons centered log-ratio
-#' transform (Aitchison 1986) for compositional data. Readcount data can be seen as compositional data
-#' since the total number of readcounts in a sample does not carry any information, but is simply
-#' an effect of sequencing depth. Thus, the information in the data lies in the relative 
-#' values, not the absolute.
+#' transform (Aitchison 1986) for compositional data. Readcount data can be seen as
+#' compositional data since the total number of readcounts in a sample does not 
+#' carry any information about the biology, but is simply an effect of sequencing
+#' depth. Thus, the information in the data lies in the relative values, not the
+#' absolute. By transforming such data with this function, you get data who are 
+#' better suited for a number of downstream analyses, e.g. typically analyses 
+#' making use of sum-of-squares type of statistics, like PCA, PLS, ANOVA or 
+#' clustering with euclidean distances.
 #' 
 #' The \code{readcount.mat} must have the samples in the rows and the taxa in the
 #' columns. Transpose if necessary.
 #' 
-#' By transforming such data with this function, you get data who are better suited for a
-#' number of downstream analyses, e.g. typically analyses making use of sum-of-squares type of
-#' statistics, like PCA, PLS, ANOVA or clustering with euclidean distances.
+#' The transform does not accept zeros in any cell of the \code{readcount.mat}. To
+#' cope with this you add pseudo-counts. Bu default 1 additional read is assigned to 
+#' all cells in \code{readcount.mat}. You may change this value, and it need not
+#' be an integer. The rationale behind this is that we a priori assume a uniform
+#' distribution of the taxa, and the more pseudo-counts you add, the more weight 
+#' you give to this prior.
 #' 
 #' @return A matrix of same size as the input, but with transformed readcounts.
 #' 
@@ -31,8 +38,12 @@
 #' 
 #' @export clr
 #' 
-clr <- function(readcount.mat, n.pseudo = 1){
-  clr.mat <- readcount.mat + n.pseudo
+clr <- function(readcount.mat, n.pseudo = 1, per.sample = FALSE){
+  if(per.sample){
+    clr.mat <- clr.mat + (n.pseudo / ncol(readcount.mat)) 
+  } else {
+    clr.mat <- readcount.mat + n.pseudo
+  }
   library.size <- rowSums(clr.mat)
   lc <- log(clr.mat)
   clr.mat <- lc - rowMeans(lc)
