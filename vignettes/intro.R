c3913838aee98fe43e59bb26ab4742231fdf5565
Lars Snipen <lars.snipen@nmbu.no>
2022-07-05 13:16:49 +0200

    Update of many help-files, but this is stil unfinished.

--- vignettes/intro.R
+++ vignettes/intro.R
@@ -4,3 +4,27 @@ KmerCount("ATGCCTGAACTGACCTGC", K = 1, col.names = TRUE)
 KmerCount("ATGCCTGAACTGACCTGC", K = 2, col.names = TRUE)
 KmerCount("ATGCCTGAACTGACCTGC", K = 3, col.names = TRUE)
 
+## ---- eval=F------------------------------------------------------------------
+#  library(microcontax)
+#  data(small.16S)
+#  tax.tab <- taxMachine(small.16S$Sequence)
+#  head(tax.tab)
+
+## ---- eval=F------------------------------------------------------------------
+#  genus <- sapply(strsplit(small.16S$Header,split=" "),function(x){x[2]})
+#  cat("Number of errors:", sum(genus != tax.tab$Genus))
+
+## ---- fig.cap = "Classification uncertainty scores", eval=F-------------------
+#  par(mfrow = c(1,2), mar = c(2.5,2,3,1))
+#  boxplot(tax.tab$D.score, factor(genus), main = "D-score")
+#  boxplot(tax.tab$R.score, factor(genus), main = "R-score")
+
+## ---- eval=F------------------------------------------------------------------
+#  tax.tab$Is.Recognized <- tax.tab$P.recognized>0.01
+#  tax.tab[35:45,]
+
+## ---- eval=F------------------------------------------------------------------
+#  rdp <- rdpTrain(small.16S$Sequence[seq(1,71,2)], genus[seq(1,71,2)])  # training step
+#  predicted <- rdpClassify(small.16S$Sequence[seq(2,71,2)], rdp)        # classification step
+#  cat( "Number of errors:", sum(predicted != genus[seq(2,71,2)]) )
+
