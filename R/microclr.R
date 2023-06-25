35cf8b44f93c422da298329a2e14d9afcec821b8
Lars Snipen <lars.snipen@nmbu.no>
2023-06-25 14:20:38 +0200

    Deleting minvarar

--- R/microclr.R
+++ R/microclr.R
@@ -38,91 +38,11 @@
 #' 
 #' @export clr
 #' 
-clr <- function(readcount.mat, n.pseudo = 1, per.sample = FALSE){
-  if(per.sample){
-    clr.mat <- clr.mat + (n.pseudo / ncol(readcount.mat)) 
-  } else {
-    clr.mat <- readcount.mat + n.pseudo
-  }
+clr <- function(readcount.mat, n.pseudo = 1){
+  clr.mat <- readcount.mat + n.pseudo
   library.size <- rowSums(clr.mat)
-  lc <- log(clr.mat)
+  lc <- log2(clr.mat)
   clr.mat <- lc - rowMeans(lc)
   attr(clr.mat, "library.size") <- library.size
   return(clr.mat)
-}
-
-#' @name minvarar
-#' @title Rarefying readcounts
-#' 
-#' @description Down-sampling readcounts with minim um variance to equal depths.
-#' 
-#' @usage rarefy(readcount.mat, depth = NULL)
-#' 
-#' @param readcount.mat matrix with readcount data.
-#' @param depth fixed number of reads in each sample after.
-#' @param rngseed Random generator seed, for reproducibility.
-#' 
-#' @details To rarefy readcounts means down-sampling the data in all samples to 
-#' a fixed target read depth. This is sometimes done prior to some diversity analyses,
-#' the idea being that some taxa are absent due to low depth, and this will bias
-#' diversity estimates. After the use \code{rarefy} all samples have equal 
-#' (low) depth.
-#' 
-#' If \code{depth = NULL} the smallest readcount among the samples is used as the
-#' target \code{depth}, but the user may supply any other positive integer value.
-#' Note: If \code{depth} is larger than the minimum readcount, some samples
-#' will be up-sampled. This means they will get more reads for the taxa where they
-#' already have reads, but still no reads for the absent taxa. 
-#' 
-#' The \code{readcount.mat} must have the samples in the rows and the taxa in the
-#' columns. Transpose if necessary.
-#' 
-#' This function minimizes the variance in the down-sampling as follows:
-#' For sample \code{i}, we first compute the expected readcount given target 
-#' \code{depth}. This is
-#' \code{Ecount = depth * read.counts[i,]/sum(read.counts[i,])}. From this we get
-#' the base count \code{base.count = floor(Ecount)}. The remainder is 
-#' \code{remainder = Ecount - base.count}. The remaining reads 
-#' \code{R = depth - sum(base.count)} are finally distributed over 
-#' the taxa using the \code{remainder} as the probabilities. Thus, only the 
-#' latter (few) reads will vary randomly between repeated use of this function
-#' on the same data.
-#' 
-#' @return A matrix of same size as the input, but with down-sampled readcounts.
-#' This matrix has an attribute \code{"original.depth"} with the original depths
-#' for each sample.
-#' 
-#' @author Lars Snipen.
-#' 
-#' @examples
-#' 
-#' @export minvarar
-#' 
-minvarar <- function(read.counts, depth = NULL, rngseed = 12345){
-  set.seed(rngseed)
-  N <- nrow(read.counts)
-  P <- ncol(read.counts)
-  samp.depths <- rowSums(read.counts)
-  if(is.null(depth)){
-    depth <- min(samp.depths)
-  }
-  X <- matrix(0, nrow = N, ncol = P)
-  taxa <- colnames(read.counts)
-  samples <- rownames(read.counts)
-  colnames(X) <- taxa
-  rownames(X) <- samples
-  cfac <- factor(1:P)
-  for(ss in 1:nrow(read.counts)){
-    Ecounts <- depth * read.counts[ss,] / samp.depths[ss]
-    counts <- floor(Ecounts)
-    remainder <- Ecounts - counts
-    if(max(remainder) == 0){
-      rest <- rep(0, P)
-    } else {
-      rest <- table(sample(cfac, size = depth - sum(counts), replace = T, prob = remainder))
-    }
-    X[ss,] <- counts + rest
-  }
-  attr(X, "original.depth") <- samp.depths
-  return(X)
 }
